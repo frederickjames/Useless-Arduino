99646006ef90e64b520350ff909f85bf69329108
James <frederickjames@gmail.com>
2020-08-28 09:27:13 -0400

    Setting up the baseline that works so I have a troubleshooting branch.

--- Useless_Arduino/Useless_Arduino.ino
+++ Useless_Arduino/Useless_Arduino.ino
@@ -1,27 +1,31 @@
 #include <Servo.h> 
 Servo servo_1; 
-int pos = 0; 
+int pos = 140; 
 int switchState; 
 void setup() 
 { 
 servo_1.attach(9); 
-pinMode(11, INPUT); 
-digitalWrite(11, HIGH); 
-servo_1.write(102);
+pinMode(10, INPUT); 
+digitalWrite(10, HIGH); 
+servo_1.write(0);
 Serial.begin(9600);
 } 
 void loop(){ 
-switchState = digitalRead(11);
+switchState = digitalRead(10);
+Serial.print("switch State:   ");
 Serial.println(switchState);
+
 if(switchState==LOW) 
 { 
-delay(50); 
-servo_1.write(0); 
-delay(300); 
-for(pos = 0; pos < 102; pos+=2){ 
-servo_1.write(pos); 
-delay(5); 
-} 
-} 
+  delay(500);  
+  for(pos = 0; pos < 180; pos+=2){ 
+    servo_1.write(pos);
+    Serial.println(servo_1.read());
+    delay(5); 
+  } 
+} /*else {
+  servo_1.write(0);
+}*/
+servo_1.write(0);
 delay(5); 
 }
